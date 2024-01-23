bee5a04fa9d811db4926e2620d5c3fe581e83296
Edward4070545 <157508300+Edward4070545@users.noreply.github.com>
2024-01-23 09:33:03 -0800

    Added Main.kt

---
+++ module01/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    //println("Hello, World!")
+    //println("My name is Edwin Corral Valenzuela Jasiel, and I'm learning Kotlin")
+    println("The hero announces her presence to the world.")
+
+    val heroName: String = "Madrigal"
+    println(heroName)
+    var playerLevel: Int = 4
+    println(playerLevel)
+
+    println("The hero embarcs on her journey to locate the enchanted sword.")
+    playerLevel += 1
+    println(playerLevel)
+}
