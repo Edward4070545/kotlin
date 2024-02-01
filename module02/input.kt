559b545fb231e8612d9cf8e3d40921181a5c400c
Edward4070545 <157508300+Edward4070545@users.noreply.github.com>
2024-02-01 09:02:56 -0800

    added input.kt

---
+++ module02/input.kt
@@ -0,0 +1,21 @@
+// HelloWorld/Task1.kt
+package helloWorldITSC1329
+
+
+fun main(){
+//TODO
+    
+    print("What day is it today? ")
+    val day = readLine()!!
+    println(day)
+    
+    
+    val first = "Edwin"
+    val last = "Corral"
+    var age = 15
+
+    println("Hello, World! My name is $first $last, and I'm $age years old")
+    println("Did I tell you that my first name is $first? ")
+    age = age - 5
+    println("And would you believe I'm $age years old?")
+}
