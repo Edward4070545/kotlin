476ddf5dd63ecdb00d0fed541be24d9a6c863d68
Edward4070545 <157508300+Edward4070545@users.noreply.github.com>
2024-03-26 08:48:05 -0700

    added assigments2.kt

---
+++ week11/assignment2.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("Number of pyramid levels:")
+    val pyramidLevels = readLine()?.toIntOrNull() ?: 0
+
+    if (pyramidLevels <= 0) {
+        println("Enter a positive integer greater than zero.")
+    } else {
+        for (i in 1..pyramidLevels) {
+            for (j in 1..i) {
+                print("*")
+            }
+            println()
+        }
+    }
+}
